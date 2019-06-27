56207deb9289387287341f94a0b01a4ebb281f17
Op14123 <51985923+Op14123@users.noreply.github.com>
2019-06-27 10:46:00 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = '/373QFb2comN8NNSoSv0+PzP2LwKumU3RcCLUw5ItmR3jb/8PskpgtshtKGaIUXG9+pubSJNVDYIA/JCeNLnNU0PAkbJEr4PGFU5GMnaXE8AARt4dm7GcWuJJgYycDXQiv+JI8FaStMOSD8amPjwuwdB04t89/1O/w1cDnyilFU=='; 
 
   $messages = [
         'type' => 'text',
