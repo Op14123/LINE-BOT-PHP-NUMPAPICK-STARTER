093cdfd447f4487b925d76adc7e6b80aa9e2c69f
Op14123 <51985923+Op14123@users.noreply.github.com>
2019-06-27 11:01:03 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = '/373QFb2comN8NNSoSv0+PzP2LwKumU3RcCLUw5ItmR3jb/8PskpgtshtKGaIUXG9+pubSJNVDYIA/JCeNLnNU0PAkbJEr4PGFU5GMnaXE8AARt4dm7GcWuJJgYycDXQiv+JI8FaStMOSD8amPjwuwdB04t89/1O/w1cDnyilFU=='; 
+ $access_token = '/373QFb2comN8NNSoSv0+PzP2LwKumU3RcCLUw5ItmR3jb/8PskpgtshtKGaIUXG9+pubSJNVDYIA/JCeNLnNU0PAkbJEr4PGFU5GMnaXE8AARt4dm7GcWuJJgYycDXQiv+JI8FaStMOSD8amPjwuwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'U7770bb8208549967593ced764fc4dad7',
+        'to' => 'U7770bb8208549967593ced764fc4dad7=',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
